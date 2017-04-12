6c78b6e2596995b2a31f23c659dd63a5cc894625
wh-Coder <461891475@qq.com>
2017-04-12 11:53:41 +0800

    复制完成

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var flash = require('connect-flash');
 var config = require('config-lite');
 var routes = require('./routes');
 var pkg = require('./package');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 var app = express();
 
@@ -56,10 +58,45 @@ app.use(function (req, res, next) {
 });
 
 
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new (winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'
+        })
+    ]
+}));
 // 路由
 routes(app);
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'
+        })
+    ]
+}));
 
-// 监听端口，启动程序
-app.listen(config.port, function () {
-    console.log(`${pkg.name} listening on port ${config.port}`);
+// error page
+app.use(function (err, req, res, next) {
+    res.render('error', {
+        error: err
+    });
 });
+
+if (module.parent) {
+    module.exports = app;
+} else {
+    // 监听端口，启动程序
+    app.listen(config.port, function () {
+        console.log(`${pkg.name} listening on port ${config.port}`);
+    });
+}
--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,27 @@
 var marked = require('marked');
 var Post = require('../lib/mongo').Post;
+var CommentModel = require('./comments');
 
+// 给 post 添加留言数 commentsCount
+Post.plugin('addCommentsCount', {
+    afterFind: function (posts) {
+        return Promise.all(posts.map(function (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
+                post.commentsCount = commentsCount;
+                return post;
+            });
+        }));
+    },
+    afterFindOne: function (post) {
+        if (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (count) {
+                post.commentsCount = count;
+                return post;
+            });
+        }
+        return post;
+    }
+});
 // 将 post 的 content 从 markdown 转换成 html
 Post.plugin('contentToHtml', {
     afterFind: function (posts) {
@@ -26,9 +47,10 @@ module.exports = {
     // 通过文章 id 获取一篇文章
     getPostById: function getPostById(postId) {
         return Post
-            .findOne({ _id: postId })
-            .populate({ path: 'author', model: 'User' })
+            .findOne({_id: postId})
+            .populate({path: 'author', model: 'User'})
             .addCreatedAt()
+            .addCommentsCount()
             .contentToHtml()
             .exec();
     },
@@ -41,9 +63,10 @@ module.exports = {
         }
         return Post
             .find(query)
-            .populate({ path: 'author', model: 'User' })
-            .sort({ _id: -1 })
+            .populate({path: 'author', model: 'User'})
+            .sort({_id: -1})
             .addCreatedAt()
+            .addCommentsCount()
             .contentToHtml()
             .exec();
     },
@@ -51,25 +74,32 @@ module.exports = {
     // 通过文章 id 给 pv 加 1
     incPv: function incPv(postId) {
         return Post
-            .update({ _id: postId }, { $inc: { pv: 1 } })
+            .update({_id: postId}, {$inc: {pv: 1}})
             .exec();
     },
 
     // 通过文章 id 获取一篇原生文章（编辑文章）
     getRawPostById: function getRawPostById(postId) {
         return Post
-            .findOne({ _id: postId })
-            .populate({ path: 'author', model: 'User' })
+            .findOne({_id: postId})
+            .populate({path: 'author', model: 'User'})
             .exec();
     },
 
-// 通过用户 id 和文章 id 更新一篇文章
+    // 通过用户 id 和文章 id 更新一篇文章
     updatePostById: function updatePostById(postId, author, data) {
-        return Post.update({ author: author, _id: postId }, { $set: data }).exec();
+        return Post.update({author: author, _id: postId}, {$set: data}).exec();
     },
 
-// 通过用户 id 和文章 id 删除一篇文章
+    // 通过用户 id 和文章 id 删除一篇文章
     delPostById: function delPostById(postId, author) {
-        return Post.remove({ author: author, _id: postId }).exec();
+        return Post.remove({ author: author, _id: postId })
+            .exec()
+            .then(function (res) {
+                // 文章删除后，再删除该文章下的所有留言
+                if (res.result.ok && res.result.n > 0) {
+                    return CommentModel.delCommentsByPostId(postId);
+                }
+            });
     }
 };
